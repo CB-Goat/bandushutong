be83aada240f0555b73dee13bfbcf439318da709
Deploy <deploy@example.com>
2026-05-11 09:13:03 +0000

    Update wsgi.py for CBGoat

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,8 @@
 import os
 import sys
 
-# 项目根目录（在 PythonAnywhere 上修改为实际路径）
-project_home = '/home/你的用户名/bandushutong'
+# 项目根目录
+project_home = '/home/CBGoat/bandushutong'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
     sys.path.insert(0, os.path.join(project_home, 'backend'))
